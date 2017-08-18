8fe5c7b0b29cdfd1603b751a6e18405cb53c24f2
Allen Royston <allen.royston@sky.uk>
2017-08-18 14:15:52 +0100

    live reload working

---
+++ src/components/helloworld/helloworld.js
@@ -0,0 +1,21 @@
+import './style.css';
+import RiotImg from './riot240x.png';
+
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = 'asfdAASDF blah \r'
+  element.classList.add('font-red');
+
+  let myIcon = new Image();
+  myIcon.src = RiotImg;
+  element.appendChild(myIcon);
+  return element;
+}
+
+document.body.appendChild(component());
+
+
+var script   = document.createElement("script");
+script.type  = "text/javascript";
+script.src   = "http://localhost:35729/livereload.js";    // use this for linked script
+document.body.appendChild(script);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,45 @@
-module.exports = options => {
-  return {
-    entry: './index.js',
-    output: {
-      filename: 'bundle.js',
-    },
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+
+module.exports = {
+  entry: {
+    helloworld: './src/components/helloworld/helloworld.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      script: 'src="http://localhost:35729/livereload.js"'
+    }),
+    new LiveReloadPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader'
+          ]
+        }
+    ]
   }
-}
+};
